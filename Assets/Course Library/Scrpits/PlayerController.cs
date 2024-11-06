3546119b9c51fa0f41e8d809fb51985e2681832f
Pirateboyfoxy <146957835+Pirateboyfoxy@users.noreply.github.com>
2024-11-06 12:45:34 -0500

    repeating background

--- Assets/Course Library/Scrpits/PlayerController.cs
+++ Assets/Course Library/Scrpits/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,8 +27,17 @@ public class PlayerController : MonoBehaviour
         }
     }   
     
-    void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over");
+        }
     }
 }
